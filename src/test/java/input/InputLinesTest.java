419f8dd00307caae220e7ad00bfe52dba3cc0c2b
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2014-08-02 12:11:11 +0200

    finished implementation of reading lines from path provided

--- src/test/java/input/InputLinesTest.java
+++ src/test/java/input/InputLinesTest.java
@@ -1,6 +1,7 @@
 package input;
 
 import static input.InputLines.LINES_WITH_ERROR;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.Assert.assertThat;
@@ -11,13 +12,16 @@ import java.nio.file.Path;
 import org.junit.Test;
 
 public class InputLinesTest {
-    private static final String EMPTY_FILE = "src\\test\\resources\\emptyFile";
-    private static final Path PATH_TO_EMPTY_FILE = getPath(EMPTY_FILE);
+    private static final Path EMPTY_FILE = getPath("emptyFile");
+    private static final Path FILE_WITH_TWO_LINES = getPath("fileWithTwoLines");
+
+    private static final String FIRST_LINE = "first line";
+    private static final String SECOND_LINE = "second line";
     private static final String FILE_NOT_FOUND = "file not found";
 
     @Test
     public void givenAPathToAnEmptyFileItReturnsEmptyLines() throws Exception {
-	InputLines lines = new InputLines(PATH_TO_EMPTY_FILE);
+	InputLines lines = new InputLines(EMPTY_FILE);
 
 	assertThat(lines.getLines(), is(empty()));
     }
@@ -30,8 +34,16 @@ public class InputLinesTest {
 	assertThat(lines.getLines().get(0), is(FILE_NOT_FOUND));
     }
 
+    @Test
+    public void givenAPathToAFileWithTwoLinesItReturnsAListOfThoseLines()
+	    throws Exception {
+	InputLines lines = new InputLines(FILE_WITH_TWO_LINES);
+
+	assertThat(lines.getLines(), hasItems(FIRST_LINE, SECOND_LINE));
+    }
+
     private static Path getPath(String filename) {
-	return new InputFile(filename).getPath();
+	return new InputFile("src\\test\\resources\\" + filename).getPath();
     }
 
     private Path pathWithIoException() {
